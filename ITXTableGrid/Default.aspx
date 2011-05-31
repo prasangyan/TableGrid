07399470a8a3b94d3d938a3567d56705535407a9
Santhosh <santhoshonet@gmail.com>
2011-05-31 19:41:17 +0530

    my initial commit to git

---
+++ ITXTableGrid/Default.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ITXTableGrid._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="style.css" rel="stylesheet" type="text/css" />
+    <script src="jquery-1.6.1.min.js" type="text/javascript"></script>
+    <script src="ITXTableGrid.js" type="text/javascript"></script>
+    <script type="text/javascript" language="javascript">
+        $(function () {
+            var Options = {
+                columnNames: ["Name", "Period", "Status", "another column1", "another column2", "another column3"],
+                data: [["some name", "some period", "some status", "some value11", "somevalue12", "some value13"],
+                       ["some name2", "some period2", "some status2", "some value21", "somevalue22", "some value23"],
+                       ["some name3", "some period3", "some status3", "some value31", "somevalue32", "some value33"]
+                      ],
+                treeview: true
+            };
+            $('.report-container').itxtablegrid(Options);
+        });
+    </script>
+</head>
+<body>
+    <div class="report-container">
+    </div>
+</body>
+</html>
---
+++ ITXTableGrid/ITXTableGrid.js
@@ -0,0 +1,50 @@
+﻿$.fn.itxtablegrid = function (opt) {
+    return this.each(function () {
+        var defaults = {
+            columnNames: [],
+            data: {},
+            treeview: false
+        };
+        var options = $.extend(defaults, opt);
+        if ($(this)[0].tagName == "DIV") {
+            var tbl = jQuery("<table>", {});
+            $(this).append(tbl);
+            // header section
+            var header = jQuery("<tr>", {});
+            if (options.columnNames.length > 0) {
+                for (var colIndex = 0; colIndex < options.columnNames.length; colIndex++) {
+                    var column = jQuery("<th>", {});
+                    column.append(options.columnNames[colIndex]);
+                    header.append(column);
+                }
+            }
+            tbl.append(header);
+            // body section
+            if (options.treeview) {
+                var BkupOptions = options.data;
+                var FilteredValues = new Array();
+                for (var rowIndex = 0; rowIndex < options.data.length; rowIndex++) {
+                    for (var colIndex = 0; colIndex < options.data[rowIndex].length; colIndex++) {
+                    }
+                }
+            }
+            else {
+                for (var rowIndex = 0; rowIndex < options.data.length; rowIndex++) {
+                    var row = jQuery("<tr>", {});
+                    for (var colIndex = 0; colIndex < options.data[rowIndex].length; colIndex++) {
+                        if (colIndex == 0) {
+                            var html = "<td><div class='expandable'><p>" + options.data[rowIndex][colIndex] + "</p></div></td>";
+                            row.append(html);
+                        }
+                        else {
+                            var col = jQuery("<td>", {});
+                            col.append(options.data[rowIndex][colIndex]);
+                            row.append(col);
+                        }
+                    }
+                    tbl.append(row);
+                }
+            }
+        }
+    });
+};
